5437a1e8437a0089f82bccf9b43d77a23003e733
obensch <o.bensch@gmx.de>
2020-02-13 17:09:20 +0100

    Add SFML to Project

---
+++ MRS/MRS.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(1280, 720), "MRS Group 3");
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+
+	return 0;
+}
